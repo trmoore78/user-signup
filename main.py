9abbb13371ca10d9d60e25767df637692ecaf89a
Torrie Moore <trmoore78@yahoo.com>
2019-04-22 20:32:37 -0500

    updated user-signup

--- main.py
+++ main.py
@@ -1,52 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
 import cgi
+import os
+import jinja2
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-page_header = """
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-5" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Validation Example</title>
-    <link rel="stylesheet" />
-  </head>
-  <body>
-"""
-
-welcomeMessage = """
-<h1>Welcome to my registration page!</h1>
-<a href="/register" value = "register_form">Site Registration</a> """
-
-
-page_footer = """
- </body>
-</html>
-"""
-
-# a registration form
-register_form = """
-<form action="/register" id="form" method="POST">
-      <h1>Register</h1>
-      <label for="username">Username</label>
-      <input type="text" name="username" id="username" value="{0}" />
-      <p class="error">{1}</p>
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password" value="{2}"/>
-      <p class="error">{3}</p>
-      <label for="password">Verify Password</label>
-      <input type="password" name="password2" id="password2" value="{4}" />
-      <p class="error">{5}</p>
-      <label for="email">Email(optional)</label>
-      <input type="text" name="email" id="email" value="{6}"/>
-      <p class="error">{7}</p>
-      <button type="submit">Register</button>
-    </form>
-"""
 
 @app.route("/register", methods=['POST'])
 def register():
@@ -54,12 +14,16 @@ def register():
     password = cgi.escape(request.form['password'])
     password2 = cgi.escape(request.form['password2'])
     email =  cgi.escape(request.form['email'])
-    emailchain = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.@"
+    emailchain = ".@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
     usernameError =""
     passwordError = ""
     password2Error =""
     emailError =""
+
+    periodcount = 0
+    spaces = 0
+    atsign = 0
 
     if not username:
         usernameError = "Username is required"
@@ -82,11 +46,34 @@ def register():
         if not hasNumber:
             passwordError = "Password must contain a number"
 
-    if password  != password2:
+    if password2  != password:
         password2Error = "Both password fields must match" 
+    elif password2 == "":
+        password2Error = "Please enter matching password" 
+    elif len(password2) == 1 or password != password2:
+        password2Error = "Please enter password that matches previous password"
+
     
     for char in email:
-        if char not in emailchain or char == " ":
+        if char == ".":
+            periodcount += 1
+
+    for char in email:
+        if char == "@":
+            atsign += 1
+    
+    for char in email:
+        if char == " ":
+            spaces += 1
+
+    for char in email:
+        if char not in emailchain:
+            emailError = "Please enter a valid email"
+        elif periodcount == 0 or periodcount > 1:
+            emailError = "Please enter a valid email"
+        elif atsign == 0 or atsign > 1:
+            emailError = "Please enter a valid email"
+        elif spaces > 0:
             emailError = "Please enter a valid email"
         elif len(email) < 3 and len(email) >= 1 or len(email) > 20:
             emailError = "Please enter a valid email"            
@@ -95,23 +82,18 @@ def register():
             
 
     if usernameError or passwordError or password2Error or passwordError or emailError:
-        content = page_header + register_form.format(username, usernameError,"", passwordError, "", password2Error, email,emailError) + page_footer
-        return content
-
-    if not usernameError or not passwordError or not password2Error or not passwordError or not emailError:
-        noError = "Thank you for registering on my page, "
-        content = page_header + noError + username + page_footer
-        return content
+        return render_template('index.html',username=username,usernameError = usernameError, password=password,passwordError = passwordError, password2=password2,password2Error = password2Error, email = email,emailError=emailError)
+    else:
+        return render_template('welcome.html',username=username)
 
 @app.route("/register", methods=['POST'])
 def register_page():
     # build the response string
-    content = welcomeMessage + register_form + page_footer
-    return content
+    return render_template('welcome.html')
 
 @app.route("/")
 def index():
-    return register_form
+    return render_template('index.html')
 
 
 app.run()
